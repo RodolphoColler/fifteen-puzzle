673c2dee13ecb796f269861bcb39bde79c9d4f39
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-01-21 17:17:10 -0300

    add simple css

--- fifteen-puzzle/src/App.jsx
+++ fifteen-puzzle/src/App.jsx
@@ -1,37 +1,41 @@
+import './Board.css'
+
 function App() {
-
-function getPartOfArray(a, index, boardSize) {
-  if (index === 0) {
-  return a.slice(0, boardSize);
-  } else {
-    return a.slice(boardSize * index, (index + 1) * boardSize);
+  function getPartOfArray(a, index, boardSize) {
+    if (index === 0) {
+    return a.slice(0, boardSize);
+    } else {
+      return a.slice(boardSize * index, (index + 1) * boardSize);
+    }
   }
-}
 
-function divideArray(array, boardSize) {
-  const arrayOfArrays = [];
-  [...Array(boardSize).keys()].forEach((_, index) => {
-    arrayOfArrays.push(getPartOfArray(array, index, boardSize))
-  })
-  return arrayOfArrays;
-}
+  function divideArray(array, boardSize) {
+    const arrayOfArrays = [];
+    [...Array(boardSize).keys()].forEach((_, index) => {
+      arrayOfArrays.push(getPartOfArray(array, index, boardSize))
+    })
+    return arrayOfArrays;
+  }
 
-function randomArray(boardSize) {
-  return [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
-}
+  function randomArray(boardSize) {
+    return [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
+  }
 
-function shuffleNumbers(boardSize) {
+  function shuffleNumbers(boardSize) {
     return divideArray(randomArray(boardSize), boardSize);
   }
 
   return (
   <>
-    <h1>Should have a board here</h1>
-    { shuffleNumbers(4).map((array, index) => array.map((number, i) => (
-      <button type="button" id={`${index}-${i}`}>
-        {number}
-      </button>
-    ))) }
+    <div class="wrapper">
+      <main class="board">
+        { shuffleNumbers(4).map((array, index) => array.map((number, i) => (
+          <button type="button" id={`${index}-${i}`}>
+            {number}
+          </button>
+        ))) }
+      </main>
+    </div>
   </>
   );
 }
