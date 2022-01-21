3a144fd18cd4fe69b0c322f005447817886ab27c
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-01-21 16:37:05 -0300

    implements shuffleNumbers

--- fifteen-puzzle/src/App.jsx
+++ fifteen-puzzle/src/App.jsx
@@ -1,7 +1,29 @@
 function App() {
 
+function partofAnArray(a, index, boardSize) {
+  if (index === 0) {
+  return a.slice(0, boardSize);
+  } else {
+    return a.slice(boardSize * index, (index + 1) * boardSize);
+  }
+}
+
+function divideArray(array, boardSize) {
+  const arrayOfArrays = [];
+  [...Array(boardSize).keys()].forEach((_, index) => {
+    arrayOfArrays.push([])
+    arrayOfArrays[index].push(partofAnArray(array, index, boardSize))
+  })
+  return arrayOfArrays;
+}
+
   function shuffleNumbers(boardSize) {
     const randomArray = [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
+
+    divideArray(randomArray)
+
+    console.log(divideArray(randomArray, boardSize));
+    
   }
   shuffleNumbers(4) 
   return (
