c067020d938fc3286b2203d2bb0583c4b48ad591
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-01-23 07:18:09 -0300

    valides if you win the game

--- fifteen-puzzle/src/App.jsx
+++ fifteen-puzzle/src/App.jsx
@@ -1,28 +1,25 @@
 import './Board.css'
 
 function App() {
-  function getPartOfArray(a, index, boardSize) {
-    if (index === 0) {
-    return a.slice(0, boardSize);
-    } else {
-      return a.slice(boardSize * index, (index + 1) * boardSize);
-    }
+
+  function getPartOfArray(array, e, boardSize) {
+    return array.slice(boardSize * e, (e + 1) * boardSize);
   }
 
   function divideArray(array, boardSize) {
     const arrayOfArrays = [];
-    [...Array(boardSize).keys()].forEach((_, index) => {
-      arrayOfArrays.push(getPartOfArray(array, index, boardSize))
+    [...Array(boardSize).keys()].forEach(e => {
+      arrayOfArrays.push(getPartOfArray(array, e, boardSize))
     })
     return arrayOfArrays;
   }
 
-  function randomArray(boardSize) {
+  function getRandomArray(boardSize) {
     return [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
   }
 
   function shuffleNumbers(boardSize) {
-    return divideArray(randomArray(boardSize), boardSize);
+    return divideArray(getRandomArray(boardSize), boardSize);
   }
 
   function getLastCharacter(string) {
@@ -33,16 +30,27 @@ function App() {
     target.classList.add('empty')
     emptyButton.classList.remove('empty')
     emptyButton.textContent = target.textContent 
-    target.textContent = ''
+    target.textContent = null
+  }
+
+  function alreadyWinTheGame() {
+    const allButtons = document.querySelectorAll('button')
+    const buttonsTextContent = [...allButtons].map(e => e.textContent)
+    const originalArray = [...Array(16).keys()]
+    buttonsTextContent.pop();
+    originalArray.shift()
+    const a  = originalArray.every((e, index) =>  e === Number(buttonsTextContent[index]))
   }
 
   function move({ target }) {
     const emptyButton = document.querySelector('.empty')
     if(getLastCharacter(emptyButton.id) === getLastCharacter(target.id) && Math.abs(emptyButton.id[0] - target.id[0]) === 1) {
       changePieces(target, emptyButton)
+      alreadyWinTheGame()
     }
     if(emptyButton.id[0] === target.id[0]  && Math.abs(getLastCharacter(emptyButton.id) - getLastCharacter(target.id)) === 1) {
       changePieces(target, emptyButton)
+      alreadyWinTheGame()
     }
   }
 
