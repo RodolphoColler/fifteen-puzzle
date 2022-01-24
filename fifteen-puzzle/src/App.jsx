b7b4124027f0016254069fcc1204c50a249da268
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-01-24 20:00:58 -0300

    add input to set board size and componentization

--- fifteen-puzzle/src/App.jsx
+++ fifteen-puzzle/src/App.jsx
@@ -1,81 +1,20 @@
 import { createSignal } from 'solid-js';
+import Board from './components/Board';
+import Input from './components/Input';
 import './Board.css'
 
 function App() {
   const [toggle, setToggle] = createSignal(false)
+  const [boardSize, setBoardSize] = createSignal(4)
 
-  function getPartOfArray(index, array, boardSize) {
-    return array.slice(boardSize * index, (index + 1) * boardSize);
-  }
-
-  function divideArray(array, boardSize) {
-    return [...Array(boardSize).keys()].map(index => getPartOfArray(index, array, boardSize))
-  }
-
-  function getRandomArray(boardSize) {
-    return [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
-  }
-
-  function shuffleNumbers(boardSize) {
-    return divideArray(getRandomArray(boardSize), boardSize);
-  }
-
-  function alreadyWinTheGame() {
-    const allButtons = document.querySelectorAll('button');
-    const buttonsTextContent = [...allButtons].map(e => e.textContent);
-    const originalArray = [...Array(4).keys()];
-    buttonsTextContent.pop();
-    originalArray.shift();
-    if(originalArray.every((e, index) =>  e === Number(buttonsTextContent[index]))) {
-      setToggle(!toggle())
-    }
-  }
-
-  function getLastCharacter(string) {
-    return string[string.length - 1];
-  }
-
-  function changePieces(target, emptyButton) {
-    target.classList.add('empty')
-    emptyButton.classList.remove('empty')
-    emptyButton.textContent = target.textContent 
-    target.textContent = null
-    alreadyWinTheGame()
-  }
-
-  function move({ target }) {
-    const emptyButton = document.querySelector('.empty')
-
-    if(getLastCharacter(emptyButton.id) === getLastCharacter(target.id) && Math.abs(emptyButton.id[0] - target.id[0]) === 1) {
-      changePieces(target, emptyButton)
-    }
-    if(emptyButton.id[0] === target.id[0]  && Math.abs(getLastCharacter(emptyButton.id) - getLastCharacter(target.id)) === 1) {
-      changePieces(target, emptyButton)
-    }
-
-  }
+  console.log(boardSize());
 
   return (
   <>
-      { toggle() && (
-        <>
-          <p>congrats</p>
-          <div class="congrats-card" />
-        </>
-      )}
     <div class={`wrapper`}>
-      <main class="board">
-        { shuffleNumbers(2).map((array, index) => array.map((number, i) => (
-          <button 
-            type="button"
-            id={`${index}-${i}`}
-            class={ number === 0 && 'empty' }
-            onClick={ (e) => move(e) }
-          >
-            {number === 0 ? null : number }
-          </button>
-        ))) }
-      </main>
+      { toggle() ? <Board boardSize={ boardSize }/>
+        : <Input setToggle={ setToggle } setBoardSize={ setBoardSize }/>
+      }
     </div>
   </>
   );
---
+++ fifteen-puzzle/src/components/Board.jsx
@@ -0,0 +1,72 @@
+// import './Board.css'
+
+function Board({ boardSize }) {
+
+  function getPartOfArray(index, array, boardSize) {
+    return array.slice(boardSize * index, (index + 1) * boardSize);
+  }
+
+  function divideArray(array, boardSize) {
+    return [...Array(boardSize).keys()].map(index => getPartOfArray(index, array, boardSize))
+  }
+
+  function getRandomArray(boardSize) {
+    return [...Array(boardSize * boardSize).keys()].sort(() => .5 - Math.random());
+  }
+
+  function shuffleNumbers(boardSize) {
+    return divideArray(getRandomArray(boardSize), boardSize);
+  }
+
+  function alreadyWinTheGame() {
+    const allButtons = document.querySelectorAll('button');
+    const buttonsTextContent = [...allButtons].map(e => e.textContent);
+    const originalArray = [...Array(4).keys()];
+    buttonsTextContent.pop();
+    originalArray.shift();
+    if(originalArray.every((e, index) =>  e === Number(buttonsTextContent[index]))) {
+      setToggle(!toggle())
+    }
+  }
+
+  function getLastCharacter(string) {
+    return string[string.length - 1];
+  }
+
+  function changePieces(target, emptyButton) {
+    target.classList.add('empty')
+    emptyButton.classList.remove('empty')
+    emptyButton.textContent = target.textContent 
+    target.textContent = null
+    alreadyWinTheGame()
+  }
+
+  function move({ target }) {
+    const emptyButton = document.querySelector('.empty')
+
+    if(getLastCharacter(emptyButton.id) === getLastCharacter(target.id) && Math.abs(emptyButton.id[0] - target.id[0]) === 1) {
+      changePieces(target, emptyButton)
+    }
+    if(emptyButton.id[0] === target.id[0]  && Math.abs(getLastCharacter(emptyButton.id) - getLastCharacter(target.id)) === 1) {
+      changePieces(target, emptyButton)
+    }
+
+  }
+
+  return (
+    <main class="board">
+    { shuffleNumbers(Number(boardSize())).map((array, index) => array.map((number, i) => (
+      <button 
+        type="button"
+        id={`${index}-${i}`}
+        class={ number === 0 && 'empty' }
+        onClick={ (e) => move(e) }
+      >
+        {number === 0 ? null : number }
+      </button>
+    ))) }
+  </main>
+  )
+}
+
+export default Board;
