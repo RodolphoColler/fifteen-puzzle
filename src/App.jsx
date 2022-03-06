dcc30fc678280a7850e028c42ed2247a134a5608
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-03-06 08:09:08 -0300

    trying removing error on deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createSignal } from 'solid-js';
 import Board from './components/Board';
 import Presentation from './components/Presentation';
-import './board.css'
 
 function App() {
   const [isTimerStarted, setIsTimerStarted] = createSignal(false)
