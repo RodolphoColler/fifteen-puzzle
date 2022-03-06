4ff271f36e287f414d7a5f78b46f4d948ef07abc
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-03-05 23:35:13 -0300

    some changes in timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,21 +1,21 @@
 import { createEffect, createSignal } from "solid-js";
 
-function Timer({isTimerStarted}) {
-  const [timer, setTimer] = createSignal('0:00');
+function Timer({ isTimerStarted, timer,setTimer }) {
   const [count, setCount] = createSignal(0);
 
   createEffect(() => {
     if(isTimerStarted()) {
       setInterval(() => {
         setCount(prev => prev + 1)
-        setTimer(`${Math.floor(count() / 60)}:${String(count() % 60).padStart(2,'0')}`)
+        setTimer(`${String(Math.floor(count() / 60)).padStart(2, '0')}:${String(count() % 60).padStart(2, '0')}`)
       }, 1000)
+
     }
   });
 
   return (
     <>
-      <h3>{ timer }</h3>
+      <h3>{ timer() }</h3>
     </>
   );
 }
